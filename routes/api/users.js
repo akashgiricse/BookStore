665691edb451a622d38965bac6c2793320d13a09
akashgiricse <akashgiricse@gmail.com>
2019-09-01 11:33:11 +0530

    Change login route

--- routes/api/users.js
+++ routes/api/users.js
@@ -82,4 +82,60 @@ router.post(
   }
 );
 
+// @route    POST api/users/login
+// @desc     Authenticate user and get token
+// @access   Public
+router.post(
+  '/login',
+  [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Please is required').exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ message: 'Invalid credentials' }] });
+      }
+
+      const passwordMatched = await bcrypt.compare(password, user.password);
+
+      if (!passwordMatched) {
+        return res
+          .status(400)
+          .json({ errors: [{ message: 'Invalid credentials' }] });
+      }
+      const payload = {
+        user: {
+          id: user.id,
+          role: user.role
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
+
 module.exports = router;
